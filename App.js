e888f924952f26b48b5398c0b18545e3d64341f4
JuanmanuelRx <juanmanuelcastillovas@gmail.com>
2025-01-20 10:23:51 +0100

    cambios alternos

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
 import { useEffect, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
-import { getProductCat } from './model/productos';
-// import { scrollProductCat } from './view/scrollerPC.js';
+import { getProductCat } from './lib/productos';
+
+const logotipo = require('./assets/lightcat.png')
+const user = require('./icons/perfil.png');
+const carrito = require('./carrito-de-compras.png');
 
 export default function App() {
     const [productos, setProductos] = useState([]);
@@ -14,29 +18,33 @@ export default function App() {
       });
     }, []);
   return (
-    <View style={styles.container}>
-      <StatusBar style="light" />
-      {
-        productos.map((product) => (
-          <View key={product.id}  style={ styles.card }>
-            <Image 
-            source= {{ uri: product.image }}
-            style={{ width: 200, height: 200, padding: 5 }}
-            />
-            <Text>Open up App.js to start working on your app!</Text>
-          </View>
-        ))
-      }
-    </View>
+    <SafeAreaProvider>
+      <View style={styles.container}>
+        <StatusBar style="light" />
+        <View style={{ backgroundColor: '#ff8000' }}>
+          <Image source={logotipo} style={{width: 300, height: 90 }}/>
+          <Image source={user} style={{width: 90, height: 90, paddingLeft: 50 }}/>
+          <Image source={carrito} style={{width: 90, height: 90, paddingRight: 10 }}/>
+        </View>
+        {productos.map((item) => (
+            <View key={item.id} style={styles.card}>
+              <Image source={{ uri: item.image }} style={{ width: '100%', height: '100%' }} />
+              <Text>{item.name}</Text>
+              <Text>Precio: ${item.price}</Text>
+            </View>
+          ))}
+      </View>
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingHorizontal: 12,
   },
   card: {
     width: 200,
